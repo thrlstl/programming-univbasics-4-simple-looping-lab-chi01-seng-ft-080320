d95e2da9681e5d585f09f5e33aec33835c65242e
Matthew Steele <matthewsteeleonline@gmail.com>
2020-07-26 03:35:28 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -39,3 +39,14 @@ def output_array(array)
         count += 1
       end
 end
+
+def return_string_array(array)
+  array = ["5", "4", "3", "2", "1"]
+  count = 0 
+  new_array = []
+    while count < array.length do
+    new_array.push(array[count].to_s)
+    count += 1 
+  end
+  new_array
+end
