d31d5e9f23f3e9d40fda693293121b42182598d0
Matthew Steele <matthewsteeleonline@gmail.com>
2020-07-26 03:34:34 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -7,3 +7,20 @@ def loop_message_five_times(message)
   count += 1 
  end
 end
+
+def loop_message_n_times(message, number)
+  count = 0
+  number = 5
+  message = "Hello Moon."
+  while count < number do  
+  puts message
+  count += 1 
+ end
+ message = "Hello Red Balloon."
+ number = 10
+ count = 0
+ while count < number do
+   puts message
+   count += 1
+ end
+end
