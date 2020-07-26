eebf0e2928880706b60ea7e54aa2dfd05cfce656
Matthew Steele <matthewsteeleonline@gmail.com>
2020-07-26 03:11:29 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,35 @@
-# Write your methods here
+def loop_message_five_times(message)
+  message = "Hello World."
+  count = 0 
+  while count < 5 do
+    puts message
+    count += 1 
+  end
+end
+
+def loop_message_n_times(message, limit)
+  message = "Hello Moon."
+  count = 0
+  limit = 5
+  while count < limit do
+    puts message
+    count += 1 
+  end
+end
+
+def output_array(message)
+  message = ["Be yourself;", "everyone else is already taken", "- Oscar Wilde"]
+  count = 0
+  while count < array.length do
+    puts array [count]
+    count += 1 
+  end
+end
+
+def return_string_array (array)
+  array = [5, 4, 3, 2, 1]
+  count = 0 
+  while count < array.length do
+    count += 1 
+  end
+end
